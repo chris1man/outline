d44a429bece303ed692a01ae3f8811203edeb4fb
chris1man <limajchris@gmail.com>
2026-09-20 07:22:17 +0700

    fix: recover partial personal files migration

--- server/migrations/20260920120000-add-personal-file-fields.js
+++ server/migrations/20260920120000-add-personal-file-fields.js
@@ -3,37 +3,55 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("attachments", "isPersonal", {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
-    });
-    await queryInterface.addColumn("attachments", "isFolder", {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
-    });
-    await queryInterface.addColumn("attachments", "parentAttachmentId", {
-      type: Sequelize.UUID,
-      allowNull: true,
-    });
-    await queryInterface.addIndex("attachments", [
-      "teamId",
-      "userId",
-      "isPersonal",
-      "parentAttachmentId",
-    ]);
+    const columns = await queryInterface.describeTable("attachments");
+    if (!columns.isPersonal) {
+      await queryInterface.addColumn("attachments", "isPersonal", {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      });
+    }
+    if (!columns.isFolder) {
+      await queryInterface.addColumn("attachments", "isFolder", {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      });
+    }
+    if (!columns.parentAttachmentId) {
+      await queryInterface.addColumn("attachments", "parentAttachmentId", {
+        type: Sequelize.UUID,
+        allowNull: true,
+      });
+    }
+    const indexName = "attachments_personal_files_index";
+    const indexes = await queryInterface.showIndex("attachments");
+    if (!indexes.some((index) => index.name === indexName)) {
+      await queryInterface.addIndex(
+        "attachments",
+        ["teamId", "userId", "isPersonal", "parentAttachmentId"],
+        { name: indexName }
+      );
+    }
   },
 
   async down(queryInterface) {
-    await queryInterface.removeIndex("attachments", [
-      "teamId",
-      "userId",
-      "isPersonal",
-      "parentAttachmentId",
-    ]);
-    await queryInterface.removeColumn("attachments", "parentAttachmentId");
-    await queryInterface.removeColumn("attachments", "isFolder");
-    await queryInterface.removeColumn("attachments", "isPersonal");
+    const columns = await queryInterface.describeTable("attachments");
+    const indexes = await queryInterface.showIndex("attachments");
+    if (indexes.some((index) => index.name === "attachments_personal_files_index")) {
+      await queryInterface.removeIndex(
+        "attachments",
+        "attachments_personal_files_index"
+      );
+    }
+    if (columns.parentAttachmentId) {
+      await queryInterface.removeColumn("attachments", "parentAttachmentId");
+    }
+    if (columns.isFolder) {
+      await queryInterface.removeColumn("attachments", "isFolder");
+    }
+    if (columns.isPersonal) {
+      await queryInterface.removeColumn("attachments", "isPersonal");
+    }
   },
 };
